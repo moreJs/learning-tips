36f250118fdbff83cc3c533c0311d29954e0b4fa
morejs <morejs@morejsdeMacBook-Air-2.local>
2016-09-19 11:19:56 +0800

    feat:deep in rxjs

--- reactive-programming/first.js
+++ reactive-programming/first.js
@@ -1,15 +1,37 @@
 'use strict';
+
 const Rx = require('rx');
 
-var subject = new Rx.Subject();
+// 只发射一次 ？
+// Observable
+// const requestStream = Rx.Observable.just('https://api.github.com/users');
+// requestStream.subscribe(url => console.log(url), err => console.log('err' + err), complete => console.log('complete'));
 
-subject.subscribe({
-  next: (v) => console.log('observerA: ' + v)
-});
-subject.subscribe({
-  next: (v) => console.log('observerB: ' + v)
-});
+const makerPlainStream = index => {
+    return Rx.Observable.create(observer => {
+        let i = index || 0;
+        const id = setInterval(() => {
+            observer.onNext(i++);
+            i == 21 && observer.completed();
+        });
+        return () => clearInterval(id);
+    });  
+};
 
-var observable = Rx.Observable.from([1, 2, 3]);
+const makerArrayStream = array => Rx.Observable.from(array || [1,2,3]);
 
-observable.subscribe(subject);
+const stream = makerPlainStream();
+const mStream = makerPlainStream(10);
+
+const Sstream = stream.map(i => i * 2);
+
+const Mstream = Sstream.merge(mStream);
+
+const ArrayStream = makerArrayStream([1, 2, 3]);
+const ArrayStream2 = makerArrayStream([11, 22, 33]);
+
+const finalArrayStream = ArrayStream.combineLatest(ArrayStream2);
+
+
+finalArrayStream.subscribe(url => {console.log(url); temp = url}, err => console.log('err' + err), complete => console.log('complete'));
+
